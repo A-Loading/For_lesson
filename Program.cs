ce83295a0b3da0ad7c695e99ad8dc05048b43625
Лобашов Андрей <a.lobashov@yanex.ru>
2023-12-25 22:36:50 +0300

    задачи 1/4 V0.1 	new file:   For_lesson.csproj 	new file:   Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class lesson2_home
+{ 
+	static void Main()
+		{
+/*//задача 1
+			int num = 161;// принимаемое число
+			Console.WriteLine($"-{num}-");
+			if ((num%7 == 0)&&(num%23 ==0))
+				{Console.WriteLine("Да");}
+			else
+				{Console.WriteLine("Нет");}
+*/ //------------------------------------|
+			
+		}
+}
