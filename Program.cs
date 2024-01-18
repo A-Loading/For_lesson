4d974e272976080bf1993bb4d16e9ff2d69b137b
Лобашов Андрей <a.lobashov@yanex.ru>
2024-01-19 00:39:00 +0300

    Сделал меню и рид ми  On branch lesson4 	modified:   Program.cs 	modified:   README.md

--- Program.cs
+++ Program.cs
@@ -1,59 +1,66 @@
 ﻿using System;
-class lesson2_home
-{ 
-	static void Main()
-		{
-/*//задача 1
-			int num = 161;// принимаемое число
-			Console.WriteLine($"-{num}-");//debag
-			if ((num%7 == 0)&&(num%23 ==0))
-				{Console.WriteLine("Да");}
-			else
-				{Console.WriteLine("Нет");}
-*/ //------------------------------------|
-/* //задача 2			
-			int x = 2;// принимаемое число x
-			int y = 1;// принимаемое число y
-			Console.WriteLine($"x-{x}-y-{y}-");//debag
-			if ((x==0)||(y==0)){
-			Console.WriteLine("Err X|Y == 0");
-			return;
-			}
-			string s;
-			if(x>0){
-				if(y>0){s="1";}
-				else{s="4";}
-			}
-			else{
-				if(y>0){s="2";}
-				else{s="3";}
-			}
-			Console.WriteLine(s);
+using System.Linq;
+
+public class BaseTools {
+	
+}
+public class tasks{
+	static public void task1()
+	{
 		
-*/ //------------------------------------|
-/* //задача 3
-			int num = 55;// принимаемое число
-			Console.WriteLine($"-{num}-");//debag
-			if ((num<10)||(num>99)){
-			Console.WriteLine("Err ≠[10..99] ");
-			return;
-			}
-			int max = num%10;
-			num /= 10;
-			if (max<num)
-				{max=num;}
-			Console.WriteLine($"{max}");
-*/ //------------------------------------|	
-//задача 4	
-			int num =-987654321;// принимаемое число
-			Console.WriteLine($"-{num}-");//debag
-			num=Math.Abs(num);
-			string s =$"{num%10}";
-			do{
-				num /=10;
-				s = $"{num%10},{s}";
-			}while((num/10) > 0);
-			Console.WriteLine(s);
-//------------------------------------|	
+	}
+}
+public class Menu {
+
+	static int PlayMenu(){
+		int n=0;
+		Console.Clear();	
+		Console.WriteLine("--MENU--");
+		Console.WriteLine("Задачи представленны по коду 1-4");
+		Console.WriteLine("Выход по коду 0");
+		Console.WriteLine("Введите код задания: ");
+        string str = Console.ReadLine();
+        n = int.Parse(str);
+		Console.Clear();
+		return(n);
+	}
+	static void GO_to_task(int a){
+		switch (a)
+			{
+            case 1:
+                tasks.task1();break;
+            case 2:
+				
+                break;
+            case 3:
+				
+				break;
+			case 4:
+				
+				break;
+			default:
+                Console.WriteLine("--ВЫХОД НА 0--");
+            break;
+        }
+	}
+	static public void Main(string[] args) {
+        int a;
+
+        if (args.Length >= 1) {
+			a = int.Parse(args[0]);
+           
+        } else {
+			a = PlayMenu();
 		}
+		GO_to_task(a);
+		
+		
+		do{
+			a = PlayMenu();
+			GO_to_task(a);
+		}while(a!=0);
+		Console.Clear();
+		Console.WriteLine("Программа завершена");
+
+    }
 }
