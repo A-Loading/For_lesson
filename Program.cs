769ceb29898e7e3b96c2e87ae1be557b20d32e3d
Лобашов Андрей <a.lobashov@yanex.ru>
2024-01-19 22:56:01 +0300

    1 задача готова  On branch lesson4 	modified:   Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;//sleep
 
 public class BaseTools {
 	public static int [] CreateRandomArray(int size, int min, int max){
@@ -14,11 +15,65 @@ public class BaseTools {
 			Console.Write($"{e} ");
 		}
 	}
-	
+	public static void ShowMesgTasks(int trash){//для повторяющихся сообщений
+		switch (trash){
+			case 1:
+				Console.WriteLine("Введите число чтобы продолжить или q для выхода");
+				Console.WriteLine("");
+			break;
+		}
+	}
+	public static bool SumDigitsIsEven(int num){//СУМ ЦИФР ЧЁТНА?
+		bool even = false;//Defolt
+		num=Math.Abs(num);//[-num]
+		
+		int sum=0; //n+u+m
+		
+			while((num/10)>0){//n u
+				sum+=num%10;
+				num/=10; 
+			}
+			
+			sum+=num; //n+u += m
+			
+			if (sum%2==0){
+				even=true; //EVEN!
+			}
+		
+		return(even);
+	}
 }
 public class tasks{
 	static public void task1(){
-		
+		BaseTools.ShowMesgTasks(1);// подсказка
+		string S_num = Console.ReadLine();
+		string memory="";//не хотел но пришлось иначе на другую сроку при вводе перекидывало
+		while(S_num != "q"){
+			int I_num;
+			Console.Clear();// чтобы ввод не мешал
+			if(int.TryParse(S_num, out I_num)){
+				if(BaseTools.SumDigitsIsEven(I_num)){
+					S_num = "q";//выход 
+				}
+				else
+				{
+					memory=$"{memory}[{S_num}] ";//не хотел но пришлось иначе на другую сроку при вводе перекидывало
+					BaseTools.ShowMesgTasks(1);// подсказка для тех кто забудет
+					Console.Write($"{memory} ");
+					S_num = Console.ReadLine();
+				}
+			}
+			else
+			{	
+				BaseTools.ShowMesgTasks(1);
+				Console.Write($"--ERR1 введено {S_num}--");
+				S_num = Console.ReadLine();
+			}
+		}
+		Console.Clear();// чтобы ввод не мешал
+		// BaseTools.ShowMesgTasks(1);
+		Console.Write($"{memory}[STOP]");
+		Thread.Sleep(3000);//чтобы увидеть результат
 	}
 	static public void task2(){
 		
@@ -38,7 +93,7 @@ public class Menu {
 		Console.WriteLine("--MENU--");
 		Console.WriteLine("Задачи представленны по коду 1-4");
 		Console.WriteLine("Выход по коду 0");
-		Console.WriteLine("Введите код задания: ");
+		Console.Write("Введите код задания: ");
         string str = Console.ReadLine();
         n = int.Parse(str);
 		Console.Clear();
